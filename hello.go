fdd8356dddf0fba7d6e2c43387fcb5064d2450bc
Gregor Riepl <Gregor.Riepl@swisstxt.ch>
2019-12-05 15:20:48 +0100

    Read app owner from environment

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"log"
 	"net/http"
@@ -22,13 +23,15 @@ func NewCounter() <-chan int {
 }
 
 func main() {
+	owner := os.Getenv("OWNER")
+
 	counter := NewCounter()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s 200", r.Host, r.Method, r.RequestURI)
 		w.Header().Add("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "<!DOCTYPE html><html><head><title>Container Demo</title></head><body><h1>Hello, %d!</h1></body></html>", <-counter)
+		fmt.Fprintf(w, "<!DOCTYPE html><html><head><title>Container Demo</title></head><body><h1>Hello, %s!</h1><p>You are visitor %d</p></body></html>", owner, <-counter)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
